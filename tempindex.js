a82557ffd2ca215fd7f6eb7155478c1293740f67
LightSpeed Fitness <lightspeedfitness@gmail.com>
2023-04-09 20:58:21 -0400

    complete lab

--- tempindex.js
+++ tempindex.js
@@ -2,28 +2,28 @@ function shout(string) {
   return string.toUpperCase();
   }
   
-function whisper(string) {
+  function whisper(string) {
   return string.toLowerCase();
   }
   
-function logShout(string) {
+  function logShout(string) {
   console.log(string.toUpperCase());
   }
   
-function logWhisper(string) {
+  function logWhisper(string) {
   console.log(string.toLowerCase());
   }
   
-function sayHiToHeadphonedRoommate(string) {
+  function sayHiToHeadphonedRoommate(string) {
   if (string.toLowerCase() === string) {
-  console.log("I can\'t hear you!");
+  console.log("I can't hear you!");
   } else if (string.toUpperCase() === string) {
   console.log("YES, INDEED!");
-  } else if (string === "Let\'s have dinner together!") {
+  } else if (string === "Let's have dinner together!") {
   console.log("I would love to!");
   } else {
-  console.log("I can\'t hear you!");
+  console.log("I can't hear you!");
   }
   }
   
-  sayHiToHeadphonedRoommate("HELLO");
+  sayHiToHeadphonedRoommate("Goodbye");
